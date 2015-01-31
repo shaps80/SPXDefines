ce8fc12e51f42735615ae77a69c5b65e13aa639b
Shaps Mohsenin <shaps@theappbusiness.com>
2015-01-31 18:03:45 +0000

    Updated pod

--- Pods/Target Support Files/Pods-SPXDefines/Pods-SPXDefines-environment.h
+++ Pods/Target Support Files/Pods-SPXDefines/Pods-SPXDefines-environment.h
@@ -9,30 +9,30 @@
 // SPXDefines
 #define COCOAPODS_POD_AVAILABLE_SPXDefines
 #define COCOAPODS_VERSION_MAJOR_SPXDefines 1
-#define COCOAPODS_VERSION_MINOR_SPXDefines 0
-#define COCOAPODS_VERSION_PATCH_SPXDefines 6
+#define COCOAPODS_VERSION_MINOR_SPXDefines 1
+#define COCOAPODS_VERSION_PATCH_SPXDefines 0
 
 // SPXDefines/Asserts
 #define COCOAPODS_POD_AVAILABLE_SPXDefines_Asserts
 #define COCOAPODS_VERSION_MAJOR_SPXDefines_Asserts 1
-#define COCOAPODS_VERSION_MINOR_SPXDefines_Asserts 0
-#define COCOAPODS_VERSION_PATCH_SPXDefines_Asserts 6
+#define COCOAPODS_VERSION_MINOR_SPXDefines_Asserts 1
+#define COCOAPODS_VERSION_PATCH_SPXDefines_Asserts 0
 
 // SPXDefines/Description
 #define COCOAPODS_POD_AVAILABLE_SPXDefines_Description
 #define COCOAPODS_VERSION_MAJOR_SPXDefines_Description 1
-#define COCOAPODS_VERSION_MINOR_SPXDefines_Description 0
-#define COCOAPODS_VERSION_PATCH_SPXDefines_Description 6
+#define COCOAPODS_VERSION_MINOR_SPXDefines_Description 1
+#define COCOAPODS_VERSION_PATCH_SPXDefines_Description 0
 
 // SPXDefines/Encoding
 #define COCOAPODS_POD_AVAILABLE_SPXDefines_Encoding
 #define COCOAPODS_VERSION_MAJOR_SPXDefines_Encoding 1
-#define COCOAPODS_VERSION_MINOR_SPXDefines_Encoding 0
-#define COCOAPODS_VERSION_PATCH_SPXDefines_Encoding 6
+#define COCOAPODS_VERSION_MINOR_SPXDefines_Encoding 1
+#define COCOAPODS_VERSION_PATCH_SPXDefines_Encoding 0
 
 // SPXDefines/Logging
 #define COCOAPODS_POD_AVAILABLE_SPXDefines_Logging
 #define COCOAPODS_VERSION_MAJOR_SPXDefines_Logging 1
-#define COCOAPODS_VERSION_MINOR_SPXDefines_Logging 0
-#define COCOAPODS_VERSION_PATCH_SPXDefines_Logging 6
+#define COCOAPODS_VERSION_MINOR_SPXDefines_Logging 1
+#define COCOAPODS_VERSION_PATCH_SPXDefines_Logging 0
 
